c50eede72175198d4c3de234904fbc8f3583a60a
Matthew Gusmano <matthew.gusmano@gmail.com>
2019-05-08 12:07:42 -0400

    added top menu component

---
+++ app/components/Popular.js
@@ -0,0 +1,22 @@
+var React = require('react');
+
+class Popular extends React.Component {
+	render() {
+		var languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', ' Python'];
+
+		return (
+			<ul className='languages'>
+				{languages.map(function (lang) {
+					return (
+						<li key= {lang}>
+						 {lang}
+						</li>
+			       )
+				})}
+			</ul>
+			
+		)
+	}
+}
+
+module.exports  = Popular;
